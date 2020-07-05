eb027ba44b0c45898ab84c71c692c790926d06a5
Steve Peterka <stevepeterka@online.de>
2020-07-05 21:18:27 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function(){ 
+
+    document.getElementById("calc").addEventListener("click", calculate); 
+}
+
+function calculate() {
+    document.getElementById("result").innerHTML = "Hello World";
+} 
